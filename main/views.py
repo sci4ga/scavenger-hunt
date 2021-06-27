8c3ba8304327b62704fdf94937238cf32a8e0251
Andrew Tsai <andrewtsai1994@gmail.com>
2021-06-27 17:05:03 -0400

    db-friendly-view first steps to making the view
    db-friendly-model
    
    db-friendly-model

--- main/views.py
+++ main/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
+from .models import Quiz, Question, QuestionOption
+from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return render(request = request,
-                  template_name='main/home.html')
+    current_quiz = ''
+    # get the current quiz the user is on
+    return render(
+        request = request,
+        template_name='main/home.html',
+        context = {
+            'user': request.user,
+            'quizzes': Quiz.objects.all
+            # get all questions for current quiz
+            # get all question options for each question that has options
+        }
+    )

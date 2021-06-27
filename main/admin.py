2564f9911282c6b28a76d8beb7921984d9f72977
Andrew Tsai <andrewtsai1994@gmail.com>
2021-06-27 17:05:03 -0400

    db-friendly-model initialized model based on standard sql model practices
    db-friendly-model
    
    db-friendly-model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Quiz, Question, QuestionOption, Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'quiz',
+        'question_number',
+        'question'
+    ]
+
+
+@admin.register(QuestionOption)
+class QuestionOptionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': [
+                'question', 
+                'option_display'
+            ]
+        }),
+        ('Value (choose one)', {
+            'fields': [
+                'text_option',
+                'number_option',
+                'integer_option'
+            ]
+        })
+    ]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': [
+                'user', 
+                'question'
+            ]
+        }),
+        ('Answer (choose one)', {
+            'fields': [
+                'option_answer',
+                'text_answer',
+                'number_answer',
+                'integer_answer'
+            ]
+        })
+    ]
+
+admin.site.register([
+    Quiz
+])
--- main/models.py
+++ main/models.py
@@ -1,3 +1,62 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+'''
+timeline, a list of quizzes, with progress through the quiz
+each quiz has a list of questions that need to be answered
+
+'''
+
+class Quiz(models.Model):
+    quiz_number = models.PositiveSmallIntegerField(primary_key = True)
+    title = models.CharField(max_length = 50, db_index = True)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(
+        to = 'Quiz',
+        on_delete = models.CASCADE
+    )
+    question_number = models.PositiveSmallIntegerField(db_index = True)
+    question = models.TextField(db_index = True)
+    
+
+class QuestionOption(models.Model):
+    question = models.ForeignKey(
+        to = 'Question',
+        on_delete = models.CASCADE
+    )
+    text_option = models.CharField(max_length = 100, db_index = True, null = True, blank = True)
+    number_option = models.DecimalField(
+        max_digits = 50,
+        decimal_places = 25,
+        db_index = True, 
+        null = True, 
+        blank = True
+    )
+    integer_option = models.IntegerField(db_index = True, null = True, blank = True)
+    option_display = models.TextField(null = True, blank = True)
+
+class Answer(models.Model):
+    user = models.ForeignKey(
+        to = settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    question = models.ForeignKey(
+        to = 'Question',
+        on_delete = models.CASCADE
+    )
+    option_answer = models.ForeignKey(
+        to = 'QuestionOption',
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+    text_answer = models.TextField(null = True, blank = True)
+    number_answer = models.DecimalField(
+        max_digits = 50,
+        decimal_places = 25,
+        null = True, 
+        blank = True
+    )
+    integer_answer = models.IntegerField(null = True, blank = True)

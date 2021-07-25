38ae07b8de3b317b447b908cb618b6dfaf18850d
Andrew Tsai <andrewtsai1994@gmail.com>
2021-07-25 15:36:03 -0400

    code-friendly-model implemented model in postgres

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -20,3 +22,34 @@ Tables:
         question ID
         response
 '''
+def answer_field():
+    return models.CharField(max_length=500, db_index=True)
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=50, db_index=True)
+    location = models.CharField(max_length=100, db_index=True)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(
+        to='Quiz', 
+        on_delete=models.CASCADE
+    )
+    question_number = models.PositiveSmallIntegerField(db_index=True)
+    question_text = models.TextField(db_index=True)
+    correct_answer = answer_field()
+    choices = ArrayField(
+        base_field=answer_field(), 
+        null=True,
+        blank=True
+    )
+
+class Response(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    question = models.ForeignKey(
+        to='Question',
+        on_delete=models.CASCADE
+    )
+    response = answer_field()

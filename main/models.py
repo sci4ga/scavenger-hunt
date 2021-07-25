cd7b9669273c591b10cbf558660db9781c2d76a0
Andrew Tsai <andrewtsai1994@gmail.com>
2021-07-25 14:04:46 -0400

    code-friendly-model added TODO

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+TODO for code friendly DB model
+
+Tables:
+    Quiz
+        Title
+        Location
+    Questions
+        quiz ID
+        question number
+        question text
+        correct answer
+        choices (array)
+    Responses
+        user ID
+        question ID
+        response
+'''

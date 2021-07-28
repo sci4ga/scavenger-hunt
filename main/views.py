9cae58aea77caf4eeae6418abf32c49a6f5c6cae
Andrew Tsai <andrewtsai1994@gmail.com>
2021-07-28 02:56:38 -0400

    code-friendly-view first steps to implementing the view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
+from .models import Quiz, Question, Response
 
 # Create your views here.
 def home(request):
     return render(request = request,
-                  template_name='main/home.html')
+                  template_name='main/home.html',
+                  context = {
+                      'quizzes': Quiz.objects.all
+                  })

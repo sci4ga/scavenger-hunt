782c3cfea838f2596af463438b3896cbcfd5ac7a
Andrew Tsai <andrewtsai1994@gmail.com>
2021-08-28 15:57:54 -0400

    code-friendly-view added new view for handling specific quizzes

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('quiz/<int:quiz_id>', views.quiz, name='quiz')
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Quiz, Question, Response
@@ -8,4 +9,19 @@ def home(request):
                   template_name='main/home.html',
                   context = {
                       'quizzes': Quiz.objects.all
+                  })
+
+def quiz(request, quiz_id):
+    try:
+        quiz = Quiz.objects.get(pk=quiz_id)
+    except Quiz.DoesNotExist:
+        raise Http404(f'Quiz with ID {quiz_id} does not exist')
+    
+    questions = quiz.question_set.all
+    return render(request = request,
+                  template_name='main/quiz.html',
+                  context = {
+                      'quizzes': Quiz.objects.all,
+                      'current_quiz': quiz,
+                      'questions': questions
                   })
